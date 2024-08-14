7a7ce252e961e3524ab51bc0b6e854c928007bb6
Sabbir Hossain Shihab <sabbirhossainrinku@gmail.com>
2024-08-15 00:21:57 +0600

    Update 14.08.2024 2

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div className='header'>
+            <div className="header-contents">
+                <h2>Order your favourite food here</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi tempora temporibus vel dolorem reiciendis est cupiditate nostrum assumenda aperiam, fuga debitis, aliquam ipsum quae culpa impedit autem distinctio non veniam.</p>
+                <button>View More</button>
+            </div>
+
+        </div>
+    )
+}
+
+export default Header
