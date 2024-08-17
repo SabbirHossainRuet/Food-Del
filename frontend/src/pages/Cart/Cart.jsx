43d0e84c91bb607a131ee9feebb60fb5bd441a45
Sabbir Hossain Shihab <sabbirhossainrinku@gmail.com>
2024-08-17 13:35:52 +0600

    Update 1 17.08.2024

--- frontend/src/pages/Cart/Cart.jsx
+++ frontend/src/pages/Cart/Cart.jsx
@@ -1,9 +1,12 @@
 import React, { useContext } from 'react'
 import './Cart.css'
 import { StoreContext } from '../../context/StoreContext'
+import { useNavigate } from 'react-router-dom'
 
 const Cart = () => {
-  // const [cartItems, addToCart, removeFromCart] = useContext(StoreContext)
+  const { cartItems, food_list, removeFromCart, getTotalCartAmount } = useContext(StoreContext)
+
+  const navigate = useNavigate();
   return (
     <div className='cart'>
       <div className="cart-items">
@@ -17,6 +20,48 @@ const Cart = () => {
         </div>
         <br />
         <hr />
+        {food_list.map((item, index) => {
+          if (cartItems[item._id] > 0) {
+            return (
+              <>
+                <div className="cart-items-title cart-items-item">
+                  <img src={item.image} alt="" />
+                  <p>{item.name}</p>
+                  <p>${item.price}</p>
+                  <p>{cartItems[item._id]}</p>
+                  <p>${item.price * cartItems[item._id]}</p>
+                  <p onClick={() => removeFromCart(item._id)} className='cross'>x</p>
+                </div>
+                <hr />
+              </>
+            )
+          }
+        })}
+      </div>
+      <div className="cart-bottom">
+        <div className="cart-total">
+          <h2>Cart Totals</h2>
+          <div>
+            <div className="cart-total-details">
+              <p>Subtotal</p>
+              <p>${getTotalCartAmount()}</p>
+            </div>
+            <hr />
+            <div className="cart-total-details">
+              <p>Delivery Fee</p>
+              <p>${getTotalCartAmount() === 0 ? 0 : 2}</p>
+            </div>
+            <hr />
+            <div className="cart-total-details">
+              <p>Total</p>
+              <p>${getTotalCartAmount() === 0 ? 0 : getTotalCartAmount() + 2}</p>
+            </div>
+          </div>
+          <button onClick={() => navigate('/order')}>PROCEED TO CHECKOUT</button>
+        </div>
+        <div className="cart-promocode">
+          <p>This is a promo code</p>
+        </div>
       </div>
     </div>
   )
